7e9eeaa8271cb32a092eb7a11f0104328f6878ba
jomujin <jomujin@gmail.com>
2022-08-18 15:25:41 +0900

    test_cordate 업데이트하라

--- tests/test_cordate.py
+++ tests/test_cordate.py
@@ -18,16 +18,16 @@ class TestClass(unittest.TestCase):
         res = self.instance.get_correct_one(self.test_date)
         self.assertEqual(res, '19880416')
 
-    # def test_load_date_dictionary(self):
-    #     res = self.instance.load_date_dictionary()
-    #     self.assertEqual(res, True)
+    def test_load_date_dictionary(self):
+        res = self.instance.load_date_dictionary()
+        self.assertEqual(res, True)
 
     def test_look_up_array(self):
+        self.instance.load_date_dictionary()
         res = self.instance.look_up_array(self.test_date)
-        print('@@@@@@@@@@@@@@@@@@@@', res[0].term)
         self.assertEqual(res[0].term, '19880416')
 
     def test_look_up_one(self):
+        self.instance.load_date_dictionary()
         res = self.instance.look_up_one(self.test_date)
-        print('@@@@@@@@@@@@@@@@@@@@', res.term)
         self.assertEqual(res.term, '19880416')
