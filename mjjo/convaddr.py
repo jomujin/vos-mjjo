b70d8a08abd21789a668e76f68532c70d9053f07
jomujin <jomujin@gmail.com>
2023-08-07 19:10:11 +0900

    convaddr _prepare 추가하고 Bjd() 를 호출하여 법정동 파일들을 읽어와서 교정 기능에 사용할 보조 데이터 생성하도록 처리하라

--- mjjo/convaddr.py
+++ mjjo/convaddr.py
@@ -1,37 +1,56 @@
 import re
 import pkg_resources
+import pandas as pd
 from typing import (
     Optional,
     List
 )
 from dataclasses import dataclass
+from mjjo.bjd import Bjd
 
 
 @dataclass
-class CorDate():
+class ConvAddr():
 
 
     def __init__(self):
+        self._prepare()
         pass
 
-def correct_simple_spacing(
-    addr: str
-) -> str:
+    def _prepare(self):
+        cls_bjd = Bjd()
+        file_name_bjd = cls_bjd.file_name_bjd
+        file_name_bjd_current = cls_bjd.file_name_bjd_current
+        file_name_bjd_changed = cls_bjd.file_name_bjd_changed
+        file_name_bjd_smallest = cls_bjd.file_name_bjd_smallest
+        file_name_bjd_frequency_dictionary = cls_bjd.file_name_bjd_frequency_dictionary
 
-    """
-    입력된 문자열(한글 주소)의 연속된 공백을 단일 공백으로 정규화한 문자열로 반환
+        self.bjd_current_dic = dict((line.split('\t')[2], line.split('\t')[9].replace('\n', '')) for line in open(file_name_bjd_current, 'r'))
+        self.bjd_smallest = [(line.strip()) for line in open(file_name_bjd_smallest, 'r')]
+        bjd_changed_df = pd.read_csv(file_name_bjd_changed, sep='\t', engine='python', encoding='utf-8')
+        old_bjd_nm_list = list(bjd_changed_df['법정동명_변경전'])
+        new_bjd_nm_list = list(bjd_changed_df['법정동명_변경후'])
+        self.bjd_changed_dic = dict((oldnm, newnm) for oldnm, newnm in zip(old_bjd_nm_list, new_bjd_nm_list))
 
-    Args:
-        addr (str): The input korean address string.
+    @staticmethod
+    def correct_simple_spacing(
+        addr: str
+    ) -> str:
 
-    Raises:
-        TypeError: If the 'addr' object is not of type string.
+        """
+        입력된 문자열(한글 주소)의 연속된 공백을 단일 공백으로 정규화한 문자열로 반환
 
-    Returns:
-        str: A string that normalize multiple consecutive spaces in a string to a single space.
-    """
+        Args:
+            addr (str): The input korean address string.
 
-    if not isinstance(addr, str):
-        raise TypeError("type of object must be string")
+        Raises:
+            TypeError: If the 'addr' object is not of type string.
 
-    return re.sub(r'\s+', ' ', addr)
+        Returns:
+            str: A string that normalize multiple consecutive spaces in a string to a single space.
+        """
+
+        if not isinstance(addr, str):
+            raise TypeError("type of object must be string")
+
+        return re.sub(r'\s+', ' ', addr)
