3714687ad699410bccb2fba11a82bbef2a6be044
jomujin <jomujin@gmail.com>
2023-08-10 17:21:39 +0900

    tests 구조를 변경하고 setup 업데이트하라

--- setup.py
+++ setup.py
@@ -38,8 +38,5 @@ setuptools.setup(
             'shortcut2 = package.module:func',
         ]
     },
-    test_suite=[
-        "tests.test_cordate",
-        "tests.test_convaddr",
-    ]
+    test_suite=["tests.tests.suite"]
 )
